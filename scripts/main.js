f000eff4afcaae997ac727cfba8b41c506184b74
isidrojavier <isidroj_fernandez@hotmail.com>
2024-03-07 17:18:05 +0100

    Dibujado rectangulo en movimiento

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,21 @@
 class Game{
     constructor(canvas, context){
         this.canvas = canvas;
-        this.context = context;
+        this.ctx = context;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+
+        this.player = new Player(this);
+    }
+
+    render(){
+        //dibujo un rectángulo
+        this.ctx.fillStyle = 'red';
+        //Una vez creado la clase player con el renderizado, podemos llamarlo
+        //en vez de ponerlo directamente aquí
+        //this.ctx.fillRect(100,200,350,150);
+        this.player.draw();
+        this.player.update();
     }
 }
 
@@ -15,4 +27,22 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext('2d');
     canvas.width = 720;
     canvas.height = 720;
+
+    //instanciamos la clase Game, que llamará a su constructor
+    //como vemos el constructor necesita dos variables que son el canvas y el contexto
+    const game = new Game(canvas, ctx);
+
+    /*creamos la ilusión del movimiento para ello debemos crear una función animate
+    que lo que va a hacer es entrar una recursividad de llamada a la función
+    para dibujar y actualizar*/
+
+    //y probamos que dibuja el rectángulo
+    function animate(){
+        //si no pongo esta línea, el rectángulo viejo no se borra
+        // y hace como una especie de barra de estado
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render();
+        requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
 });
--- scripts/player.js
+++ scripts/player.js
@@ -1,9 +1,20 @@
 class Player{
     constructor(game){
         this.game = game;
-        this.x = 0;
-        this.y = 0;
-        this.width = 100;
-        this.height = 100;
+        this.x = 50;
+        this.y = 60;
+        this.width = 200;
+        this.height = 200;
+    }
+
+    //construyo el método del jugador que le dibuja
+    //le voy a llamar draw por diferenciar
+    draw(){
+        this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    //construyo el método update que hará que se mueva un pixel a la derecha
+    update(){
+        this.x++;
     }
 }
